7193fc0167f6353f66f1d85f24add4d5e609d03e
moishemg <moishemg@gmail.com>
2014-09-18 12:55:40 +0000

    retoques de color

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140917080356) do
+ActiveRecord::Schema.define(version: 20140918121739) do
+
+  create_table "comidas", force: true do |t|
+    t.string   "cuerpo"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "compras", force: true do |t|
     t.string   "cuerpo"
