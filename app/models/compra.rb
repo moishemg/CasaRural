c6a583ffba239d51ae738123a44c54f6a8346fb5
moishemg <moishemg@gmail.com>
2014-09-19 11:57:37 +0000

    validates_uniqueness_of :cuerpo

--- app/models/compra.rb
+++ app/models/compra.rb
@@ -1,8 +1,10 @@
 class Compra < ActiveRecord::Base
-  validates_presence_of :cuerpo
+  validates_presence_of :cuerpo, :message => '. Debes introducir una compra.'
+  validates_uniqueness_of :cuerpo, :message => '. Ya existe una compra con el mismo nombre.'
+  
   def esComprado
     if comprado
-      return "Sí"
+      return ""
     else
       return "PTE"
     end
